64b9f8d75d29fc823a478d5431f7f4ad37ed75ec
Yidi Wang <alexdwang@hotmail.com>
2018-02-21 18:33:20 -0700

    add combobox

--- Main.py
+++ Main.py
@@ -2,8 +2,8 @@ import matplotlib.pyplot as plt
 import os
 import tkinter.filedialog as filedialog
 from tkinter import *
-from tkinter.ttk import *
-# from itertools import cycle
+from tkinter import ttk
+from itertools import cycle
 import importlib
 
 from GUI import execute, NetListGenerator, Library
@@ -20,36 +20,32 @@ class Interface(object):
         self.label_parts = Label(self.window, text='parts:', font=('Arial', 0), width=16, height=2)
         self.parts_options = StringVar()
         self.parts_options_tuple = (Library.PARTS)
-        self.parts_options.set(self.parts_options_tuple)
-        self.lb_parts = Listbox(self.window, listvariable=self.parts_options, height=6, exportselection=False)
+        self.cb_parts = ttk.Combobox(self.window, textvariable=self.parts_options, exportselection=False, state='readonly')
+        self.cb_parts['values'] = self.parts_options_tuple
 
         self.label_simulation = Label(self.window, text='simulation:', font=('Arial', 0), width=16, height=2)
         self.simulation_options = StringVar()
         self.simulation_options_tuple = (Library.SIMULATION)
-        self.simulation_options.set(self.simulation_options_tuple)
-        self.lb_simulation = Listbox(self.window, listvariable=self.simulation_options,
-                                             height=6, exportselection=False)
+        self.cb_simulation = ttk.Combobox(self.window, textvariable=self.simulation_options, exportselection=False, state='readonly')
+        self.cb_simulation['values'] = self.simulation_options_tuple
 
         self.label_TID_level = Label(self.window, text='TID level:', font=('Arial', 0), width=16, height=2)
         self.TID_options = StringVar()
         self.TID_options_tuple = ()
-        self.TID_options.set(self.TID_options_tuple)
-        self.lb_TID = Listbox(self.window, listvariable=self.TID_options,
-                                      height=len(self.TID_options_tuple), exportselection=False)
+        self.cb_TID = ttk.Combobox(self.window, textvariable=self.TID_options, exportselection=False, state='readonly')
+        self.cb_TID['values'] = self.TID_options_tuple
 
         self.label_output_x = Label(self.window, text='X Label:', font=('Arial', 0), width=16, height=2)
         self.output_options_x = StringVar()
         self.output_options_tuple_x = ()
-        self.output_options_x.set(self.output_options_tuple_x)
-        self.lb_output_x = Listbox(self.window, listvariable=self.output_options_x,
-                                           height=len(self.output_options_tuple_x), exportselection=False)
+        self.cb_output_x = ttk.Combobox(self.window, textvariable=self.output_options_x, exportselection=False, state='readonly')
+        self.cb_output_x['values'] = self.output_options_tuple_x
 
         self.label_output_y = Label(self.window, text='Y Label:', font=('Arial', 0), width=16, height=2)
         self.output_options_y = StringVar()
         self.output_options_tuple_y = ()
-        self.output_options_y.set(self.output_options_tuple_y)
-        self.lb_output_y = Listbox(self.window, listvariable=self.output_options_y,
-                                         height=len(self.output_options_tuple_y), exportselection=False)
+        self.cb_output_y = ttk.Combobox(self.window, textvariable=self.output_options_y, exportselection=False, state='readonly')
+        self.cb_output_y['values'] = self.output_options_tuple_y
 
         self.button_import = Button(self.window, text='Import', width=15, height=2, command=self.import_hit)
         self.button_execute = Button(self.window, text='Execute', width=15, height=2, command=self.execute_hit)
@@ -65,6 +61,8 @@ class Interface(object):
     def import_hit(self):
         filename = filedialog.askopenfilename(initialdir=relative_path('Netlist'))
         # print(filename)
+        if filename == '':
+            return
         f = open(filename, 'r')
         line = next(f)
         while line[0:5] != 'Title':
@@ -142,13 +140,6 @@ class Interface(object):
                     line = next(f)
             line = next(f, None)
         f.close()
-        # print(my_part)
-        # print(my_voltage_source)
-        # print(my_circuit_core)
-        # print(my_input)
-        # print(my_output)
-        # print(my_subcircuit)
-        # print(my_library)
         Library.PARTS.append(my_part)
         Library.PARTS = list(set(Library.PARTS))
         Library.save_name_to_json(Library.TITLE, Library.PARTS, Library.SIMULATION, Library.TID_LEVEL, Library.TID_LIST,
@@ -172,15 +163,11 @@ class Interface(object):
     def execute_hit(self):
         # try:
         if self.input_check():
-            part = self.lb_parts.get(self.lb_parts.curselection())
-            simulation = self.lb_simulation.get(self.lb_simulation.curselection())
-            TID_level = self.lb_TID.get(self.lb_TID.curselection())
-            output_option_x = self.lb_output_x.get(self.lb_output_x.curselection())
-            output_option_y = self.lb_output_y.get(self.lb_output_y.curselection())
-            # if (part == Library.PART_LT1175):
-            #     output_option = self.lb_output.get(self.lb_output.curselection())
-            # else:
-            #     output_option = Library.AD590_OUTPUT_OPTION[0]
+            part = self.cb_parts.get()
+            simulation = self.cb_simulation.get()
+            TID_level = self.cb_TID.get()
+            output_option_x = self.cb_output_x.get()
+            output_option_y = self.cb_output_y.get()
             self.output_filepath = relative_path('Output/' + part + '_' + TID_level + '_test.txt')
 
             netListGenerator.generate(part, simulation, TID_level, output_option_x, output_option_y, self.output_filepath, self.netlist_filepath)
@@ -200,50 +187,43 @@ class Interface(object):
     def refresh(self):
         importlib.reload(Library)
         self.parts_options_tuple = (Library.PARTS)
-        self.parts_options.set(self.parts_options_tuple)
-        self.lb_parts.selection_clear(0, len(self.parts_options_tuple))
+        self.parts_options.set('')
+        self.cb_parts['values'] = self.parts_options_tuple
         self.simulation_options_tuple = (Library.SIMULATION)
-        self.simulation_options.set(self.simulation_options_tuple)
-        self.lb_simulation.select_clear(0, len(self.simulation_options_tuple))
-        self.TID_options_tuple = ()
-        self.TID_options.set(self.TID_options_tuple)
-        self.output_options_tuple_x = ()
-        self.output_options_x.set(self.output_options_tuple_x)
-        self.output_options_tuple_y = ()
-        self.output_options_y.set(self.output_options_tuple_y)
+        self.simulation_options.set('')
+        self.cb_simulation['values'] = self.simulation_options_tuple
+        self.TID_options.set('')
+        self.output_options_x.set('')
+        self.output_options_y.set('')
         self.result_text.set('')
         return
 
     def input_check(self):
-        if self.lb_TID.curselection() == () or \
-                self.lb_parts.curselection() == () or \
-                self.lb_simulation.curselection() == () or \
-                self.lb_output_x.curselection() == () or \
-                self.lb_output_y.curselection() == ():
+        if self.cb_TID.get() == () or \
+                self.cb_parts.get() == () or \
+                self.cb_simulation.get() == () or \
+                self.cb_output_x.get() == () or \
+                self.cb_output_y.get() == ():
             self.result_text.set('please check your input')
             return False
         self.result_text.set('in process, please wait...')
         return True
 
     def TID_option_update(self):
-        cur_sim = self.lb_simulation.get(self.lb_simulation.curselection())
+        cur_sim = self.cb_simulation.get()
         self.TID_options_tuple = Library.TID_LIST[cur_sim]
-        self.TID_options.set(self.TID_options_tuple)
+        self.TID_options.set('')
+        self.cb_TID['values'] = self.TID_options_tuple
 
     def part_onselect(self, evt):
         try:
-            index = int(self.lb_parts.curselection()[0])
-            part = self.lb_parts.get(index)
+            part = self.cb_parts.get()
             self.output_options_tuple_x = (Library.OUTPUT_OPTION[part])
-            self.output_options_x.set(self.output_options_tuple_x)
+            self.cb_output_x['values'] = self.output_options_tuple_x
+            self.output_options_x.set('')
             self.output_options_tuple_y = (Library.OUTPUT_OPTION[part])
-            self.output_options_y.set(self.output_options_tuple_y)
-            # if value == Library.PART_LT1175:
-            #     self.label_output.grid()
-            #     self.lb_output.grid()
-            # elif value == Library.PART_AD590:
-            #     self.label_output.grid_remove()
-            #     self.lb_output.grid_remove()
+            self.cb_output_y['values'] = self.output_options_tuple_y
+            self.output_options_y.set('')
         except:
             pass
         return
@@ -263,30 +243,35 @@ class Interface(object):
         self.label_parts.grid(row=1)
         self.label_simulation.grid(row=1, column=1)
         self.label_TID_level.grid(row=1, column=2)
-        self.label_output_x.grid(row=1, column=3)
-        self.label_output_y.grid(row=1, column=4)
 
         # row 2
-        self.lb_parts.grid(row=2)
-        self.lb_simulation.grid(row=2, column=1)
-        self.lb_TID.grid(row=2, column=2)
-        self.lb_output_x.grid(row=2, column=3)
-        self.lb_output_y.grid(row=2, column=4)
+        self.cb_parts.grid(row=2)
+        self.cb_simulation.grid(row=2, column=1)
+        self.cb_TID.grid(row=2, column=2)
 
         # row 3
-        self.button_import.grid(row=3, column=0)
-        self.button_execute.grid(row=3, sticky='E', columnspan=3, pady=10)
 
         # row 4
-        self.label_result_text.grid(row=4, column=0, columnspan=3)
+        self.label_output_x.grid(row=4, column=0)
+        self.label_output_y.grid(row=4, column=1)
+
+        # row 5
+        self.cb_output_x.grid(row=5, column=0)
+        self.cb_output_y.grid(row=5, column=1)
+
+        # row 6
+
+        # row 7
+        self.button_import.grid(row=7, column=0)
+        self.button_execute.grid(row=7, sticky='E', columnspan=3, pady=10)
+
+        # row 8
+        self.label_result_text.grid(row=8, column=0, columnspan=3)
+
 
         # bind onselect function with widget
-        self.lb_parts.bind('<<ListboxSelect>>', self.part_onselect)
-        self.lb_simulation.bind('<<ListboxSelect>>', self.simulation_onselect)
-
-        # remove output option listbox
-        # self.label_output_x.grid_remove()
-        # self.lb_output_x.grid_remove()
+        self.cb_parts.bind('<<ComboboxSelected>>', self.part_onselect)
+        self.cb_simulation.bind('<<ComboboxSelected>>', self.simulation_onselect)
 
         self.window.mainloop()
         return
